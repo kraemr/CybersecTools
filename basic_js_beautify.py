6e63e466cf6c531637e0b4c4fd284681f14425b5
Robin Krämer <93094049+kraemr@users.noreply.github.com>
2023-09-19 15:01:17 +0200

    Create basic_js_beautify.py

---
+++ basic_js_beautify.py
@@ -0,0 +1,26 @@
+def add_newline_after_braces(input_file):
+    try:
+        with open(input_file, 'r') as f:
+            content = f.read()
+    except FileNotFoundError:
+        print(f"Error: File '{input_file}' not found.")
+        return
+
+    output_content = ''
+    indent_level = 0
+
+    for char in content:
+        if char == '{':
+            output_content += char
+            indent_level += 1
+        elif char == '}':
+            output_content += '}\n' + '  ' * (indent_level - 1)  # Add newline and proper indentation
+            indent_level -= 1
+        else:
+            output_content += char
+
+    print(output_content)
+
+if __name__ == "__main__":
+    input_file = input("Enter the input file name: ")
+    add_newline_after_braces(input_file)
