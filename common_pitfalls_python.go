5b7e1573f816b25e8264355a982f6b8b31b83636
Robin Krämer <93094049+kraemr@users.noreply.github.com>
2023-09-23 17:16:44 +0200

    Add files via upload

--- common_pitfalls_python.go
+++ common_pitfalls_python.go
@@ -14,17 +14,64 @@ if there is an alias for pickle called p then key "pickle" --> "p"
 */
 
 
-// check if vulnerable input is delivered to a function like pickle.loads() or os.system() either directly or indirectly --> "Some String %s" % uservar 
+/*
+the flow of the program is as follows:
+first we read file into mem
+then we read our wordlists
+then we analyze the lines:
+
+i.e
+p.loads("stuff" % var) 
+
+detection from wordlist: "%s".loads(, pkgname   %s is alias, then we just split on ,
+then we just print the line number and the vuln
+*/
+
+
+
+// check if vulnerable input is delivered to a function like pickle.loads() or os.system() either directly or indirectly --> "Some String %s" % uservar
 func checkFuncArgs(){
 
 }
 
-// This gets called whenever an import is encountered and adds it to to a global obj
-func resolveImports(){
-
+// This gets called whenever an import is encountered and adds it to to a global map
+func resolveImport(importstr  string) {
+	splitted := strings.Split(importstr," ")
+	if len(splitted) < 2 || !strings.Contains(importstr, "import") {
+		return
+	}
+	if splitted[0] == "import" { // we are importing a package
+		found_pkg_name := false // if this is a valid pkg import
+		has_alias := false
+		pkg_name := ""
+		alias := ""
+		for i:=1; i<len(splitted); i++{
+			if splitted[i] != " " && found_pkg_name == false{
+				pkg_name = splitted[i]
+				found_pkg_name = true
+				continue
+			}
+			if splitted[i] != " " && found_pkg_name && splitted[i] == "as" {
+				has_alias = true
+				for j:=i; j<len(splitted);j++{
+					if splitted[j] != " "{
+						alias = splitted[j]
+					}
+				}
+			}
+		}
+		if has_alias==true{
+			g_import_aliases[pkg_name] = alias
+		}else {
+			g_import_aliases[pkg_name] = pkg_name
+		}
+		fmt.Println(g_import_aliases[pkg_name])
+	}
+	
 }
 
 // regex maybe ??
+// basically youll have a wordlist with format strings, where the formatted part gets replaced with the resolved pkg name
 func checkForKeywords(str string,keywords []string,line int) bool{
 	for i:=0; i< len(keywords); i++ {
 		if strings.Contains(str,keywords[i]) {
@@ -34,6 +81,9 @@ func checkForKeywords(str string,keywords []string,line int) bool{
 	return false
 }
 
+
+
+// check if single quote string
 func isFormatStringInjectable(str string, isDjango bool) bool {
 	splitted := strings.Split(str,"\"")
 	if len(splitted) < 3 {
@@ -55,7 +105,7 @@ func readFileIntoStringBuf(filestr string) ([]string,error){
     defer f.Close()
     sc := bufio.NewScanner(f)
     for sc.Scan() {
-        lines = append(lines, sc.Text()) 
+        lines = append(lines, sc.Text())
 	}
 	return lines,sc.Err()
 }
@@ -79,14 +129,15 @@ func main(){
 	    return
 	}
 
-	//  read in list of function names and formatted strings like: "%sfuncname" where %s is either empty or class. 
-	//  before that try resolving imports 
+	//  read in list of function names and formatted strings like: "%sfuncname" where %s is either empty or class.
+	//  before that try resolving imports
 	for i:=0; i<len(buffer); i++{
 		line := buffer[i]
+		_ = line
 		// First check for vuln funcs like os's .system, os.writefile pickle.loads, djangos template.render
 	}
 
-	b := isFormatStringInjectable("\"test %s\" % variable",false)
-	fmt.Println(b)
+	//b := isFormatStringInjectable("\"test %s\" % variable",false)
+	resolveImport("import pkg as p")
 	return
 }
