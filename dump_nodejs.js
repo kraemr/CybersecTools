efe0ed78342e9bccae64e9f24a79cc049c7e7af1
Robin Krämer <93094049+kraemr@users.noreply.github.com>
2023-11-02 09:44:51 +0100

    Delete dump_nodejs.js

--- dump_nodejs.js
+++
@@ -1,4 +0,0 @@
-// you can use this to write strings to a file
-require('fs').writeFileSync("/tmp/pwned","test")
-// or this if its an object
-require('fs').writeFileSync("/tmp/pwned",require('util').inspect(Object))
